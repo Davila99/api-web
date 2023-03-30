7b891716cc37572bd1754edf6faed723eccd1606
Davila99 <davilaeliseo453@gmail.com>
2023-03-29 21:06:15 -0600

    Update categorias.service.ts

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm/dist';
+import { Repository } from 'typeorm';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { Categoria } from './entities/categoria.entity';
 
 @Injectable()
 export class CategoriasService {
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
   create(createCategoriaDto: CreateCategoriaDto) {
     return 'This action adds a new categoria';
   }
 
-  findAll() {
-    return `This action returns all categorias`;
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaRepository.find();
   }
 
   findOne(id: number) {
