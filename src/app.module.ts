24c7454c05dd236f5310b7263e83b28458f81be5
Davila99 <davilaeliseo453@gmail.com>
2023-03-29 20:39:45 -0600

    configurando el CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
+import { Categoria } from './categorias/entities/categoria.entity';
 
 @Module({
-  imports: [CategoriasModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [Categoria],
+    synchronize: true,
+  }),CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/categorias/entities/categoria.entity.ts
+++ src/categorias/entities/categoria.entity.ts
@@ -1,1 +1,13 @@
-export class Categoria {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
